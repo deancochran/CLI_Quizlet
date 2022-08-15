44a3219ac08a05b70b1e22e889d0407a2c8c3b9c
Dean Cochran <charles.dean.cochran@outlook.com>
2022-08-15 23:53:43 +0200

    untested update

--- cli_quizlet.py
+++ cli_quizlet.py
@@ -1,3 +1,4 @@
+from fileinput import filename
 import random
 import json
 import os
@@ -5,26 +6,12 @@ import time
 import datetime
 
 class Flashcard():
-    def __init__(self, q=None, a=None, obj=None):
-        if obj is None:
-            assert type(q) is str
-            assert type(a) is str
-            self.q = q
-            self.a = a
-            self.side = 0
-        else:
-            assert type(obj) is dict
-            self.q = obj['q']
-            self.a = obj['a']
-            self.side = 0
+    def __init__(self,obj: dict):
+        self.q = obj['q']
+        self.a = obj['a']
         self.skips=0
         self.flips=0
-
-    def set_q(self, question):
-        self.q=question
-
-    def set_a(self, answer):
-        self.a = answer
+        self.side=0
 
     def flip(self):
         if self.side == 0:
@@ -32,119 +19,105 @@ class Flashcard():
         else:
             self.side = 0
 
+    def set_q(self, question):
+        self.q=question
+
+    def set_a(self, answer):
+        self.a = answer
+
     def show(self):
         if self.side == 0:
             print(f'Question: {self.q}')
         else:
             print(f'Answer: {self.a}')
 
-    def read_json(self, obj):
-        self.question = obj['q']
-        self.answer = obj['a']
-
     def to_json(self):
-        return {
-            'q':self.q,
-            'a':self.a
-            }
+        return {'q':self.q, 'a':self.a}
 
-
-
-
-class Deck():
-    def __init__(self, decks_dir, name='deck'):
-        self.decks_dir=decks_dir
-        self.name = name
-        self.deck = {}
+    def save(self, flashcards_path, card_filename):
+        file_path = os.path.join(flashcards_path, card_filename)
+        with open(file_path, "w") as f:
+            f.write(json.dumps(self.to_json()))
+    
+class Deck(): 
+    def __init__(self,flashcard_folders_dir):
+        self.flashcard_folders_dir=flashcard_folders_dir
+        self.load_flashcard_path()
 
     @property
     def size(self):
-        return len(self.deck)
+        return len(self.deck.keys())
 
-    def items(self):
-        return self.deck.items()
+    def get_card(self,card_name: str):
+        card_path = os.path.join(self.flashcards_path, card_name)
+        obj=json.loads(open(card_path,"rb").read())
+        return Flashcard(obj)
+    
+    def add_card(self, obj):
+        assert obj is dict
+        card = Flashcard(obj)
+        card.save(self.flashcards_path, f'card_{self.size}')
 
-    def add_card(self, card: Flashcard):
-        self.deck[self.size] = card
+    def remove_card(self, card_name):
+        card_path = os.path.join(self.flashcards_path, card_name)
+        os.remove(card_path)
 
-    def get_card(self, bad_keys=None):
-        if bad_keys is None:
-            keys = list(self.deck.keys())
-            card_key = random.sample(keys, 1)[0]
-            return card_key, self.deck[card_key]
-        else:
-            keys = [key for key in list(self.deck.keys()) if key not in bad_keys]
-            card_key = random.sample(keys, 1)[0]
-            return card_key, self.deck[card_key]
+    def load_flashcard_path(self):
+        assert os.path.exists(self.flashcard_folders_dir) == True
+        assert len(os.listdir(self.flashcard_folders_dir)) > 0
+        available_decks={filename.split('.')[0]: filename for filename in os.listdir(self.flashcards_dir)}
+        flag=False
+        while flag==False:
+            print('Available Decks: ',list(available_decks.keys()))
+            name = str(input('Enter a Deck Name: '))
+            if name in list(available_decks.keys()):
+                self.name=name
+                flag=True
+        self.flashcards_path = f'{self.flashcards_dir}/{available_decks[name]}'
+        print(f'Loaded Deck: {self.name} from {self.flashcards_dir}, Size: {self.size}')
 
-    def remove_card(self, card_key):
-        self.deck.pop(card_key)
-
-    def is_empty(self):
-        if self.deck == {}:
-            return True
-        else:
-            return False
-
-    def load_flashcards(self, flashcards_path):
-        flashcard_filenames = os.listdir(flashcards_path)
-        for filename in flashcard_filenames:
-            card_path=flashcards_path+f'/{filename}'
-            obj=json.loads(open(card_path,"rb").read())
-            card=Flashcard(obj=obj)
-            self.add_card(card)
-        print(f'Loaded Deck from {flashcards_path}')
-
-    def load_deck(self, deck_path):
-        self.name=deck_path.split("/")[-1].split(".")[0]
-        obj=json.loads(open(deck_path,"rb").read())
-        for _, v in obj.items():
-            self.add_card(Flashcard(obj=v))
-        print(f'Loaded Deck from {deck_path}')
-        
-    def save_deck(self):
-        if not os.path.exists(self.decks_dir):
-            os.makedirs(self.decks_dir)
-        filename=self.decks_dir+f"/{self.name}.json"
-        with open(filename, "w") as f:
-            f.write(json.dumps(self.to_json()))
-        print(f'Saved Deck to {self.decks_dir}')
-
-    def to_json(self):
-        return {k:v.to_json() for k,v in self.items()}
-            
 class Quiz():
     def __init__(self, deck: Deck, results_dir):
         self.deck=deck
         self.results_dir=results_dir
         self.start_size=self.deck.size       
         self.answered=0
-        self.current_card_key,self.current_card = deck.get_card()
-        self.removed_card_keys=[]
-        self.show_cli()
+        self.first_card_shown=False
+        self.cards=[]
+        self.answered_cards=[]
+        self.load_deck()
+        self.start()
         self.show_quiz()
-
-    def show_cli(self):
-        os.system ('clear')
-        print('Starting Flashcard quiz...')
-        print(f'Deck: {self.deck.name} Size: {self.start_size} cards')
-        time.sleep(2)
-        self.start_time=datetime.datetime.now()
         
-    def action_keys_info(self):
+    def get_rand_card(self):
+        return random.sample(self.cards, 1)[0]
+        
+    def show_action_keys(self):
         print(f"""Action Keys: skip == " enter ", flip== " ' ", remove == " ; " """)
 
-    def quiz_status(self):
+    def show_quiz_status(self):
         complete=self.answered/self.start_size
         print(f'Finished {complete:.2f}% of Deck: "{self.deck.name}"')
 
-    def show_current_card(self):
-        self.current_card.show()
+    def load_deck(self):
+        for card_name in os.listdir(self.deck.flashcards_path):
+            self.cards.append(self.deck.get_card(card_name))
 
+    def start(self):
+        for i in reversed(range(5)):
+            os.system ('clear')
+            if i == 0:
+                print(f'Deck: {self.deck.name} Size: {self.start_size} cards')
+                print(f'Starting Flashcard quiz in.. {i}')
+            else:
+                print(f'GO!')
+            time.sleep(1)
+        self.start_time=datetime.datetime.now()
+    
     def skip(self):
         self.key_press=True
         self.current_card.skips+=1
-        self.current_card_key,self.current_card = self.deck.get_card(bad_keys=self.removed_card_keys)
+        self.current_card = self.get_rand_card()
         self.show_quiz()
         
     def flip_card(self):
@@ -156,12 +129,12 @@ class Quiz():
     def remove_card(self):
         self.key_press=True
         self.answered+=1
-        self.removed_card_keys.append(self.current_card_key)
-        if len(self.removed_card_keys) != self.start_size:
-            self.current_card_key,self.current_card = self.deck.get_card(bad_keys=self.removed_card_keys)
+        self.answered_cards.append(self.cards.pop(self.cards.index(self.current_card)))
+        if len(self.answered) != self.start_size:
+            self.current_card = self.get_rand_card()
             self.show_quiz()
         else:
-            self.end_time=datetime.datetime.now()
+            self.end_time=datetime.datetime.now()   
             self.show_quiz_results()
 
     def get_input(self):
@@ -176,20 +149,31 @@ class Quiz():
             print('\r',end='')
             self.get_input()
 
+    def show_quiz(self):
+        os.system ('clear')
+        self.show_quiz_status()
+        self.show_action_keys()
+        print(' ')
+        self.current_card.show()
+        self.get_input()
+
     def show_quiz_results(self):
         os.system ('clear')
-        self.quiz_status()
+        self.show_quiz_status()
         self.total_time = self.end_time-self.start_time
         print(f"Time: {self.total_time}")
         self.save_quiz_results()
-        
-    def show_quiz(self):
-        os.system ('clear')
-        self.quiz_status()
-        self.action_keys_info()
-        print(' ')
-        self.show_current_card()
-        self.get_input()
+
+    def to_json(self):
+        data={}
+        for card in self.answered_cards:
+            data[card.q]={
+                    'answer':card.a,
+                    'flips':card.flips,
+                    'skips':card.skips,
+                }
+        data['time']=str(self.total_time)
+        return data
 
     def save_quiz_results(self):
         if not os.path.exists(self.results_dir):
@@ -200,21 +184,9 @@ class Quiz():
             f.write(json.dumps(self.to_json()))
         print(f'Saved Quiz to {quiz_results_file_path}')
 
-    def to_json(self):
-        data={}
-        for card_id, card in self.deck.items():
-            data[card_id]={
-                    'question':card.q,
-                    'answer':card.a,
-                    'flips':card.flips,
-                    'skips':card.skips,
-                }
-        data['time']=str(self.total_time)
-        return data
-
 if __name__ == "__main__":
     os.system('clear')
-    deck=Deck(decks_dir='decks', name="" )
+    deck=Deck(decks_dir='decks')
     decks={filename.split('.')[0]: filename for filename in os.listdir('decks')}
     flag=False
     while flag==False:
@@ -223,4 +195,5 @@ if __name__ == "__main__":
         if name in list(decks.keys()):
             flag=True
     deck.load_deck(f'decks/{decks[name]}')
+    time.sleep(5)
     Quiz(deck, 'results')
