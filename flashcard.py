f7ec9aa3f3ad406ca6558c3c0711f9e5a5680a92
Dean Cochran <deancochran@MacDean.home>
2022-09-18 13:52:22 +0200

    v2 rebase

--- flashcard.py
+++ flashcard.py
@@ -1,31 +1,31 @@
-import json
 import os
-import time
-
 class Flashcard():
-    def __init__(self,obj: dict, flashcards_path:str, card_index:int):
-        self.flashcards_path=flashcards_path
-        self.card_index=card_index
-        self.q = obj['q']
-        self.a = obj['a']
-        self.skips=0
+    """
+        Flashcard is a class that represents a single question and answer set which can be identified by
+        its unique uuid identifier
+    """
+    def __init__(self, id, obj:dict):
+        self.id = str(id)
+        self.q = str(obj['q'])
+        self.a = str(obj['a'])
         self.flips=0
-        self.side=0
+        self.skips=0
+        self.side = 0
     
-    def get_q(self):
+    def get_q(self) -> str:
         return self.q
 
-    def get_a(self):
+    def get_a(self) -> str:
         return self.a
 
+    def get_id(self) -> str:
+        return self.id
+
     def set_q(self, question):
-        self.q=question
+        self.q = question
 
     def set_a(self, answer):
         self.a = answer
-
-    def set_card_index(self, card_index:int):
-        self.card_index = card_index
 
     def flip(self):
         if self.side == 0:
@@ -33,25 +33,40 @@ class Flashcard():
         else:
             self.side = 0
 
+    def to_json(self):
+        return {'id':self.id, 'qa':{'q':self.q, 'a':self.a}}
+
     def show(self):
         if self.side == 0:
             print(f'Question: {self.q}')
         else:
             print(f'Answer: {self.a}')
+       
+    def edit(self):
+        self.show_editor() 
+        done=False
+        while done==False:
+            key = str(input(f'Enter a Action Key: '))
+            if key == "":
+                done=True
+            elif key.lower() == "a":
+                self.set_a(self.prompt_user(key='a'))
+                self.show_editor() 
+            elif key.lower() == "q":
+                self.set_q(self.prompt_user(key='q'))
+                self.show_editor() 
+            else:
+                self.show_editor()
 
-    def to_json(self):
-        return {'q':self.q, 'a':self.a}
+    def show_editor(self):
+        os.system('clear')
+        print('Card Editor View')
+        print('_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _')
+        print('Question: ', self.q)
+        print('Answer: ', self.a)
+        print('_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _')
+        print(f"""Action Keys: quit == " enter ", edit Answer == " A ", edit Question == " Q " """)
 
-    def save(self):
-        file_path = os.path.join(self.flashcards_path, f"card_{self.card_index}"+'.json')
-        print('attempting to save card', file_path)
-        with open(file_path, "w") as f:
-            f.write(json.dumps(self.to_json()))
-
-    def remove(self):
-        card_path = os.path.join(self.flashcards_path, f"card_{self.card_index}"+'.json')
-        print('attempting to remove card', card_path)
-        os.remove(card_path)
     def prompt_user(self, key='q'):
         if key == 'q':
             keyword='Question'
@@ -72,30 +87,3 @@ class Flashcard():
                 else:
                     print('\r\r', end='')
         return qa
-
-    def show_editor(self):
-        os.system('clear')
-        print('Card Editor View')
-        print('_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _')
-        print('Question: ', self.q)
-        print('Answer: ', self.a)
-        print('_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _')
-        print(f"""Action Keys: quit == " enter ", edit Answer == " A ", edit Question == " Q " """)
-
-    def edit(self):
-        self.show_editor() 
-        done=False
-        while done==False:
-            key = str(input(f'Enter a Action Key: '))
-            if key == "":
-                done=True
-            elif key.lower() == "a":
-                self.set_a(self.prompt_user(key='a'))
-                self.show_editor() 
-            elif key.lower() == "q":
-                self.set_q(self.prompt_user(key='q'))
-                self.show_editor() 
-            else:
-                self.show_editor()
-
-        
